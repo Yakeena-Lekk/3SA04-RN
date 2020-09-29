f4043cbe575fb3966f6a61a09f28dc60a1206d58
Yakeena <s593512074@phuket.psu.ac.th>
2020-09-30 03:43:07 +0700

    Hello World

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const doIt = () => {
+    console.log("Hello from console")
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text onPress={doIt}>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
