0c553697924b0368982c04faceddacd83f1dd136
Yakeena <s593512074@phuket.psu.ac.th>
2020-09-30 23:01:05 +0700

    Connect

--- components/Weather.js
+++ components/Weather.js
@@ -1,18 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState ,useEffect} from 'react';
 import { View, Text, ImageBackground, StyleSheet } from 'react-native';
 import Forecast from './Forecast';
+
+const apiKey = 'ae2b10c1fab1416e7d5a5ffa25fa45f4'
 
 export default function Weather(props) {
     const [forecastInfo, setForecastInfo] = useState({
         main: 'main',
         description: 'description',
-        temp: 0
+        temp: 0,
     })
+
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {              
+   
+   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=${apiKey}`)
+                   .then((response) => response.json())
+                   .then((json) => {
+                       setForecastInfo({
+                           main: json.weather[0].main,
+                           description: json.weather[0].description,
+                           temp: json.main.temp
+                       });
+                   })
+                   .catch((error) => {
+                       console.warn(error);
+                   });
+           }
+       }, [props.zipCode])   
+
     return (
         <View>
             <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
                 <View style={styles.cover}>
-                <Text>Zip Code</Text>
                 <Text style= {styles.small}> zipCode is {props.zipCode}</Text>
                 <Forecast {...forecastInfo} />
                 </View>
